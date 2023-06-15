919e0a45fa5a6fbd1ef54a1798e076bc2608f45c
shayma-hammami <hammami.shayma111@gmail.com>
2023-06-15 13:03:58 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - a program that print a line using printf function 
- * Return: 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

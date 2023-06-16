3dc6971274945e7b1e57f04e8f71d66fd6ec3e54
shayma-hammami <hammami.shayma111@gmail.com>
2023-06-16 15:36:18 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Main function
+ *Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for(ch = 'a' ; ch <= 'z'; ch++)
+		putchar (ch);
+	return (0);
+}
+

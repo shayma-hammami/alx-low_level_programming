1f42ff8d5072eda9062f10db64a315f9c024689b
shayma-hammami <hammami.shayma111@gmail.com>
2023-06-16 17:07:40 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point 
+ *Return: 0 Always (success)
+ */
+
+int main() {
+int i;
+char letter;
+
+for (i = 0 ; i >= 9 ; i++) 
+	putchar('0' + i);
+for (letter = 'a'; letter <= 'f'; letter++)
+        putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
+
